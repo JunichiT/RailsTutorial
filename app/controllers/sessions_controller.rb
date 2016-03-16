0587325eb05126b3845aa9a47c5149cf2d2dba1d
j.tsurukawa <j.tsurukawa@gmail.com>
2016-03-16 19:31:08 +0900

    Add user login test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # login and redirect to user page
+      log_in user
+      redirect_to user
     else
       # show err message
       flash.now[:danger] = 'Invalid email/password combination'
